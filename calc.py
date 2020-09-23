5d553d2dbbcb1f46c530e354a73617bd5f895312
Moth-S <gse182@yandex.ru>
2020-09-23 21:00:00 +0300

    first try

---
+++ calc.py
@@ -0,0 +1,16 @@
+# module calc
+
+print('-----------------------------')
+print('Добро пожаловать в мой модуль')
+x = 10
+def sum(a, b):
+	return a + b
+
+def sub(a, b):
+	return a - b
+
+def mult(a, b):
+	return a * b
+
+def div(a, b):
+	return a / b
